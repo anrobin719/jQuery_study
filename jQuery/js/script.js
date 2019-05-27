7ca8b4e34dac1bb5c64b4320424d4e84bca9c5b9
RobinAn <anrobin719@gmail.com>
2019-05-28 08:41:09 +0900

    jQuery to toggle

--- jQuery/js/script.js
+++ jQuery/js/script.js
@@ -9,33 +9,51 @@ $(function() {
 //    $(":header").css("border","1px solid red");
 //    $("div:contains('Brad')").css("border","1px solid red");
     
-    $("#box").click(function() {
-        alert("you just click the box");
+//    $("#box").click(function() {
+//        alert("you just click the box");
+//    });
+//    
+//    $("input").blur(function() {
+//        if($(this).val() == "") {
+//            $(this).css("border","solid 1px red");
+//            $("#box").text("forget to add text");
+//        }
+//    });
+//    
+//    $("input").keydown(function() {
+//        if($(this).val() !== "") {
+//            $(this).css("border","solid 1px #777");
+//            $("#box").text("Thanks for that");
+//        }
+//    });
+//    
+//    $("#box").hover(function() {
+//        $(this).css("background-color", "powderblue");
+//        $(this).text("You hovered in!");
+//        $(this).css("transition", "all 3s ease-in-out");
+//    }, function() {
+//        $(this).css("background-color", "orange");
+//        $(this).text("You hovered out!");
+//        
+//    });
+//    
+//    $(".notification-bar").delay(1000).slideDown().delay(1000).fadeOut();
+
+    
+//    $("div.hidden").show();
+    $("div.hidden").slideDown(500);
+    $("#box1").click(function () {
+        $(this).fadeTo(1000, 0.25, function() {
+            $(this).slideUp();
+        });
     });
     
-    $("input").blur(function() {
-        if($(this).val() == "") {
-            $(this).css("border","solid 1px red");
-            $("#box").text("forget to add text");
-        }
+    $("button").click(function() {
+        $("#box1").slideToggle();
     });
     
-    $("input").keydown(function() {
-        if($(this).val() !== "") {
-            $(this).css("border","solid 1px #777");
-            $("#box").text("Thanks for that");
-        }
-    });
     
-    $("#box").hover(function() {
-        $(this).css("background-color", "powderblue");
-        $(this).text("You hovered in!");
-        $(this).css("transition", "all 3s ease-in-out");
-    }, function() {
-        $(this).css("background-color", "orange");
-        $(this).text("You hovered out!");
-        
-    });
+    
     
     
     
